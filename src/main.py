293b21dddd69937f8350e612c3315947e86b27fb
latimize <194050601+latimize@users.noreply.github.com>
2026-05-01 19:11:38 +0400

    add build script and update repo settings

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 import os
 import shutil
+import sys
 from textnode import TextNode
 from htmlnode import HTMLNode
 from blocks import markdown_to_html_node, extract_title
+
+
+if len(sys.argv) == 0:
+    basepath = "/"
+else:
+    basepath = sys.argv[0]
+
 
 def reset_directory(dir):
     if os.path.exists(dir):
@@ -19,10 +27,10 @@ def recursive_copy(source, destination):
         print(f" * {full_path} -> {os.path.join(destination, element)}")
 
 def copy_static():
-    reset_directory("./public")
-    recursive_copy("./static", "./public")
+    reset_directory("./docs")
+    recursive_copy("./static", "./docs")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as from_file:
         from_content = from_file.read()
@@ -30,22 +38,23 @@ def generate_page(from_path, template_path, dest_path):
         template_content = template_file.read()
     html_content = markdown_to_html_node(from_content).to_html()
     page_title = extract_title(from_content)
-    full_html_page = template_content.replace("{{ Title }}", page_title).replace("{{ Content }}", html_content)
+    html_page = template_content.replace("{{ Title }}", page_title).replace("{{ Content }}", html_content)
+    full_html_page = html_page.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as f:
         f.write(full_html_page)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for path in os.listdir(dir_path_content):
         full_path = os.path.join(dir_path_content, path)
         if os.path.isfile(full_path):
             full_dest_path = os.path.join(dest_dir_path, path).replace(".md", ".html")
-            generate_page(full_path, template_path, full_dest_path)
+            generate_page(full_path, template_path, full_dest_path, basepath)
         elif os.path.isdir(full_path):
-            generate_pages_recursive(full_path, template_path, os.path.join(dest_dir_path, path))
+            generate_pages_recursive(full_path, template_path, os.path.join(dest_dir_path, path), basepath)
 
 def main():
     copy_static()
-    generate_pages_recursive("./content", "template.html", "./public")
+    generate_pages_recursive("./content", "template.html", "./docs", basepath)
 
 main()
